190bba42d19a1b29ba2ef2dce1b89eabf911e889
JuanMurilloo <emmanuel.velezme@gmail.com>
2023-08-15 18:25:51 -0500

    arrays js

--- Ejemplos/index.js
+++ Ejemplos/index.js
@@ -10,11 +10,11 @@ const materiaPreferida = { nombre: "Calculo", id: 0 }
 const universidad = null
 const carrera = undefined;
 
-console.log("Tipo de dato de nombre" + typeof(nombre))
+console.log("Tipo de dato de nombre" + typeof (nombre))
 const precioMatricula = "5555.555"
-console.log("Tipo de dato de precioMatricula " + typeof(precioMatricula))
+console.log("Tipo de dato de precioMatricula " + typeof (precioMatricula))
 const precioMatricula2 = parseInt(precioMatricula)
-console.log("Tipo de dato de precioMatricula2 " + typeof([precioMatricula2]))
+console.log("Tipo de dato de precioMatricula2 " + typeof ([precioMatricula2]))
 
 console.log(`${nombre} ${apellido}`)
 
@@ -22,8 +22,8 @@ console.log("------operador ternario-------")
 console.log(edad >= 18 ? "Es mayor de edad" : "Es menor de edad");
 
 let condition = edad > 18
-if (condition) {}
-if (condition) {} else {}
+if (condition) { }
+if (condition) { } else { }
 
 const dia = "MARTES"
 switch (dia) {
@@ -39,14 +39,14 @@ switch (dia) {
 }
 
 console.log("------funciones-------")
-function imprimirNombre(){
+function imprimirNombre() {
     console.log(nombre)
 }
 
 imprimirNombre();
 
 const imprimirNombreyApellido = (nombre, apellido) => {
- console.log(`${nombre} ${apellido}`);   
+    console.log(`${nombre} ${apellido}`);
 }
 
 imprimirNombreyApellido(nombre, apellido)
@@ -55,7 +55,7 @@ const crearNombreCompleto = (nombre, apellido) => {
     console.log("--GLOBAL SCOPE FAKE--");
     console.log(nombre, apellido);
     let name = nombre
-    if(nombre.length >3){
+    if (nombre.length > 3) {
         let name = nombre[0] + " " + apellido
         console.log(name);
     }
@@ -63,3 +63,37 @@ const crearNombreCompleto = (nombre, apellido) => {
 }
 
 crearNombreCompleto(nombre, apellido)
+
+
+//ARRAY
+const estudiantes = []
+//agregar
+//estudiantes.push(5)
+//estudiantes.push(null)
+//estudiantes.push({})
+//estudiantes.push(()=>{console.log();})
+
+estudiantes.push({ "nombre": "Juan", id: 12 })
+estudiantes.push({ "nombre": "Samuel", id: 23 })
+estudiantes.push({ "nombre": "Juan", id: 34 })
+console.log(estudiantes);
+
+
+
+estudiantes.forEach((x) =>
+    console.log(x.nombre));
+
+for (let i = 0; i < estudiantes.length; i++) {
+    if (estudiantes[i].nombre == "Juan"){
+        console.log(estudiantes[i].id)
+    }
+}
+
+const Juanes = estudiantes.filter(x=>x.nombre == "Juan")
+console.log(Juanes);
+
+const Juane = estudiantes.find(x=>x.nombre == "Juan")
+console.log(Juane);
+
+let estudiantes2 = estudiantes.map(x => {return{...x, edad:null}})
+console.log(estudiantes2)
